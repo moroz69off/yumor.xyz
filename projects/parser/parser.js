aa21498554e3ad2316deb037fb7cf02e1ec645dd
moroz69off <moroz69off@gmail.com>
2022-05-04 23:43:22 +0300

    template script
    Signed-off-by: moroz69off <moroz69off@gmail.com>

--- projects/parser/parser.js
+++ projects/parser/parser.js
@@ -9,18 +9,30 @@
 var data = [];
 var result = null;
 var mrz_parser = document.createElement("div");
+var domain_value = "contoso.ru";
+var input_value = "tele-vizor";
 
 Init();
 
 function Init () {
 	window.onload = function () {
 
+		let domain_name = domain_value;
+		
+		let path = input_value;
 
-
-		result = 100;
+		result = GetResult(domain_name, path);
 
 		mrz_parser.innerHTML = '<p>parse result: ' + result + '<ruby><rt>₽</rt><rp>ruble</rp></ruby></p>';
 
 		document.body.appendChild(mrz_parser);
 	}
+}
+
+function GetResult (arg1, arg2) {
+
+	let data_value = ["arg1", "arg2"];
+	data[0] = data_value[0].length;
+	data[1] = data_value[1].length;
+	return (data[0] + data[1]) * 10 * 10 * 10 * 10;
 }
