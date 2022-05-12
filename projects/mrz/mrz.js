4ce7b23b9ddc31c0767b8433a998dc0c4947cc63
Морозов Юрий <moroz69off@gmail.com>
2022-05-12 10:55:38 +0300

    repair test

--- projects/mrz/mrz.js
+++ projects/mrz/mrz.js
@@ -9,56 +9,55 @@
 var data = [];
 var result = null;
 var mrz_ = document.createElement("div");
-var domain_value = null;
-var responce_value = null;
+var domain_value = "a";
+var responce_value = "b";
 
 Init();
 
-function Init () {
+function Init() {
 	window.onload = function () {
+
 		mrz_.classList.add('result');
 		mrz_.classList.add('container');
-		mrz_.style.color = '#c0c1c2';
+		mrz_.style.color = '#c9c9c9';
+
 		document.body.appendChild(mrz_);
 	}
 }
 
 function GetResult() {
-	result = domain_value.Length + responce_value.Length;
-	if (result) {
-		mrz_.innerHTML = '<p>Result: ' + result + '₽</p>';
-	}
-	return null;
-}
-function GetResult () {
-	let xhr = new XMLHttpRequest();
+	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 
-	xhr.open('GET', domain_value);
+	var xhr = new XHR();
 
-	xhr.send();
+	xhr.open('GET', domain_value, true);
 
-	xhr.onload = function() {
+	xhr.onload = function () {
 		if (xhr.status != 200) {
-			console.log( 'Error: ' + xhr.status);
+			console.log('Error: ' + xhr.status);
 			return;
 		}
 	};
 
-	xhr.onprogress = function(event) {
+	xhr.onprogress = function (event) {
 		console.log(`loaded ${event.loaded} из ${event.total}`);
 	};
 
-	xhr.onerror = function() {
-		alert(xhr.status);
+	xhr.onerror = function () {
+		alert('Error: ' + xhr.status);
 	};
+
+	xhr.send();
+
 	result = xhr.response;
+
 	mrz_.innerHTML = '<div class="container-fluid"><p>Result: ' + result + '₽</p></div>';
 }
 
-function GetDomain (event) {
+function GetDomain(event) {
 	domain_value = event.target.value;
 }
 
-function GetResponce (event) {
+function GetResponce(event) {
 	responce_value = event.target.value;
 }
